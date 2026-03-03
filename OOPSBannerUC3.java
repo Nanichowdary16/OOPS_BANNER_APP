a91c6109fbc344ff895b1e09526e810b645573aa
Nanichowdary16 <naninunna7378@gmail.com>
2026-03-03 10:09:29 +0530

    adding file and code

---
+++ OOPSBannerUC3.java
@@ -0,0 +1,18 @@
+public class OOPSBannerUC3 {
+    public class OOPSBannerUC3 {
+    public static void main(String[] args) {
+
+        System.out.println(String.join("\n",
+
+                String.join("", "  *****  ", "  *****  ", " ******  ", "  *****  "),
+                String.join("", " *     * ", " *     * ", " *     * ", " *     * "),
+                String.join("", "*       *", "*       *", " *     * ", "*        "),
+                String.join("", "*       *", "*       *", " ******  ", "  *****  "),
+                String.join("", "*       *", "*       *", " *       ", "       * "),
+                String.join("", " *     * ", " *     * ", " *       ", " *     * "),
+                String.join("", "  *****  ", "  *****  ", " *       ", "  *****  ")
+
+        ));
+    }
+}
+}
